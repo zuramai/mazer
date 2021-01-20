1110bc06d98abf5309439773b31b21b7e61714a4
zuramai <ahmadsaugigis@yahoo.com>
2021-01-20 15:07:22 +0700

    :bug: fix: sidebar dropdown animation

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,3 +1,5 @@
+function slideToggle(t,e,o){0===t.clientHeight?j(t,e,o,!0):j(t,e,o)}function slideUp(t,e,o){j(t,e,o)}function slideDown(t,e,o){j(t,e,o,!0)}function j(t,e,o,i){void 0===e&&(e=400),void 0===i&&(i=!1),t.style.overflow="hidden",i&&(t.style.display="block");var p,l=window.getComputedStyle(t),n=parseFloat(l.getPropertyValue("height")),a=parseFloat(l.getPropertyValue("padding-top")),s=parseFloat(l.getPropertyValue("padding-bottom")),r=parseFloat(l.getPropertyValue("margin-top")),d=parseFloat(l.getPropertyValue("margin-bottom")),g=n/e,y=a/e,m=s/e,u=r/e,h=d/e;window.requestAnimationFrame(function l(x){void 0===p&&(p=x);var f=x-p;i?(t.style.height=g*f+"px",t.style.paddingTop=y*f+"px",t.style.paddingBottom=m*f+"px",t.style.marginTop=u*f+"px",t.style.marginBottom=h*f+"px"):(t.style.height=n-g*f+"px",t.style.paddingTop=a-y*f+"px",t.style.paddingBottom=s-m*f+"px",t.style.marginTop=r-u*f+"px",t.style.marginBottom=d-h*f+"px"),f>=e?(t.style.height="",t.style.paddingTop="",t.style.paddingBottom="",t.style.marginTop="",t.style.marginBottom="",t.style.overflow="",i||(t.style.display="none"),"function"==typeof o&&o()):window.requestAnimationFrame(l)})}
+
 let sidebarItems = document.querySelectorAll('.sidebar-item.has-sub');
 for(var i = 0; i < sidebarItems.length; i++) {
     let sidebarItem = sidebarItems[i];
@@ -5,9 +7,9 @@ for(var i = 0; i < sidebarItems.length; i++) {
         e.preventDefault();
         
         let submenu = sidebarItem.querySelector('.submenu');
-
-        if(submenu.classList.contains('active')) submenu.classList.remove('active');
-        else submenu.classList.add('active');
+        if(submenu.style.display == 'none') submenu.classList.add('active')
+        else submenu.classList.remove('active')
+        slideToggle(submenu, 300)
     })
 }
 
