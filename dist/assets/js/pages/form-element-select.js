64cdf887af5157b65fc0476a6ce5402c8ce08edf
zuramai <ahmadsaugigis@yahoo.com>
2021-08-02 15:05:44 +0700

    fix(#24): choices.js default max 5 items

---
+++ dist/assets/js/pages/form-element-select.js
@@ -0,0 +1,15 @@
+let choices = document.querySelectorAll('.choices');
+let initChoice;
+for(let i=0; i<choices.length;i++) {
+  if (choices[i].classList.contains("multiple-remove")) {
+    initChoice = new Choices(choices[i],
+      {
+        delimiter: ',',
+        editItems: true,
+        maxItemCount: -1,
+        removeItemButton: true,
+      });
+  }else{
+    initChoice = new Choices(choices[i]);
+  }
+}
