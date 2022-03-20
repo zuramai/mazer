14ddac53261531a4b13abd261dd869ed393ff82d
NikB <nik.baedorf@gmail.com>
2022-03-21 00:05:34 +0100

    Fix tooltips

--- src/assets/js/mazer.js
+++ src/assets/js/mazer.js
@@ -1,8 +1,12 @@
-//Bootstrap JS
-require('bootstrap');
+// Bootstrap JS
+const bootstrap = require('bootstrap');
 
-//We could import PerfectScrollbar directly in the sidebar module
+// We could import PerfectScrollbar directly in the sidebar module
 window.PerfectScrollbar = require('perfect-scrollbar/dist/perfect-scrollbar.min.js');
+
+// Make bootstrap's js available via the window object
+// Used in the tooltip example
+window.bootstrap = bootstrap;
 
 let Sidebar  = require('./components/sidebar');
 
