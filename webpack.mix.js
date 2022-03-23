4914eb264c7d46e432b17412a04b7e91d8d57391
bl0cknumber <developerfauzan@gmail.com>
2022-03-23 19:19:53 +0700

    meta: `markup` is null by default
    Rel: https://github.com/moyus/laravel-mix-nunjucks/blob/8d6b3e589dbe068648fdb86b4738240fe07d487b/src/NunjucksTask.js#L26

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,13 +20,8 @@ mix
     processCssUrls: false,
   });
 
-// mix.browserSync({
-//     proxy: 'mazer.test',
-// });
-
 mix.njk("src/*.html", "dist/", {
   ext: ".html",
-  marked: null,
   watch: true,
   data: {
     web_title: "Mazer Admin Dashboard",
