9efa17e911b61264bf92cdf8f7462e0613105316
Ø <developerfauzan@gmail.com>
2022-10-01 21:57:48 +0700

    fix: flickering dark mode theme, closes #118 (#130)
    * fix: flickering dark mode theme, closes #118
    
    * fix: do not defer bootstrap instead use the minified version of bootstrap

--- src/assets/js/bootstrap.js
+++ src/assets/js/bootstrap.js
@@ -1,5 +1,5 @@
 // Bootstrap's JS'
-const bootstrap = require('bootstrap');
+const bootstrap = require('bootstrap/dist/js/bootstrap.min.js');
 
 // Make bootstrap's js available via the window object
 // (Used in the tooltip example)
--- src/assets/js/components/dark.js
+++ src/assets/js/components/dark.js
@@ -14,6 +14,7 @@ export function toggleDarkTheme() {
 /**
  * Set theme for mazer
  * @param {"theme-dark"|"theme-light"} theme
+ * @param {boolean} dontPersist 
  */
 export function setTheme(theme, dontPersist = false) {
   document.body.className = document.body.className.replace(THEME_REGEX, "")
@@ -31,7 +32,10 @@ if (toggler) {
   })
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+/**
+ * Init theme from setTheme()
+ */
+function initTheme() {
   //If the user manually set a theme, we'll load that
   let storedTheme
   if ((storedTheme = localStorage.getItem(THEME_KEY))) {
@@ -52,4 +56,12 @@ document.addEventListener("DOMContentLoaded", () => {
   )
 
   return setTheme(mediaQuery.matches ? "theme-dark" : "theme-light", true)
-})
+}
+
+if (document.readyState === 'loading') {
+    // Document not yet loaded, so wait for it.
+    window.addEventListener('DOMContentLoaded', initTheme);
+} else {
+    // Document is ready (interactive or complete), so call init immediately.
+    initTheme();
+}
