3e1da7d26a0215353a90f52628d6ee9909219732
zuramai <ahmad.saugi@binus.ac.id>
2023-01-16 01:13:27 +0000

    update: demo

--- demo/assets/js/initTheme.js
+++ demo/assets/js/initTheme.js
@@ -1,1 +1,1 @@
-(()=>{var e=document.body;"theme-dark"==localStorage.getItem("theme")?e.classList.add("theme-dark"):e.classList.add("theme-light")})();
+(()=>{document.body;var e=localStorage.getItem("theme");e&&document.documentElement.setAttribute("data-bs-theme",e)})();
