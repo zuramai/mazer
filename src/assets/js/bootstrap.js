0370d0c332ae39a1d3523b78cffd130f8719abdc
zuramai <saugi.dev@gmail.com>
2023-03-28 13:23:33 +0700

    refactor: move `window.bootstrap`

--- src/assets/js/bootstrap.js
+++
@@ -1,6 +0,0 @@
-// // Bootstrap's JS'
-// const bootstrap = require('bootstrap/dist/js/bootstrap.min.js');
-
-// // Make bootstrap's js available via the window object
-// // (Used in the tooltip example)
-// window.bootstrap = bootstrap;
--- src/assets/js/mazer.js
+++ src/assets/js/mazer.js
@@ -1,6 +1,7 @@
 // Don't forget to load bootstrap js in your project
 // Here, we load it in our app.js
-import "bootstrap/dist/js/bootstrap.bundle"
+import bootstrapBundle from "bootstrap/dist/js/bootstrap.bundle"
+window.bootstrap = bootstrapBundle
 
 // We could import PerfectScrollbar directly in the sidebar module
 import "./../static/js/components/sidebar"
