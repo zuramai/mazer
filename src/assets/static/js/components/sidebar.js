7150c5f34c73368199b739adece5043c7796de12
zuramai <saugi.dev@gmail.com>
2023-03-30 05:59:52 +0700

    feat: multi-level menu

--- src/assets/static/js/components/sidebar.js
+++ src/assets/static/js/components/sidebar.js
@@ -1,7 +1,5 @@
 import isDesktop from '../helper/isDesktop'
 
-// function slideToggle(a, b, c) { 0 === a.clientHeight ? j(a, b, c, !0) : j(a, b, c) } function slideUp(a, b, c) { j(a, b, c) } function slideDown(a, b, c) { j(a, b, c, !0) } function j(c, a, k, d) { void 0 === a && (a = 400), void 0 === d && (d = !1), c.style.overflow = "hidden", d && (c.style.display = "block"); var l, b = window.getComputedStyle(c), e = parseFloat(b.getPropertyValue("height")), f = parseFloat(b.getPropertyValue("padding-top")), g = parseFloat(b.getPropertyValue("padding-bottom")), h = parseFloat(b.getPropertyValue("margin-top")), i = parseFloat(b.getPropertyValue("margin-bottom")), m = e / a, n = f / a, o = g / a, p = h / a, q = i / a; window.requestAnimationFrame(function s(r) { void 0 === l && (l = r); var b = r - l; d ? (c.style.height = m * b + "px", c.style.paddingTop = n * b + "px", c.style.paddingBottom = o * b + "px", c.style.marginTop = p * b + "px", c.style.marginBottom = q * b + "px") : (c.style.height = e - m * b + "px", c.style.paddingTop = f - n * b + "px", c.style.paddingBottom = g - o * b + "px", c.style.marginTop = h - p * b + "px", c.style.marginBottom = i - q * b + "px"), b >= a ? (c.style.height = "", c.style.paddingTop = "", c.style.paddingBottom = "", c.style.marginTop = "", c.style.marginBottom = "", c.style.overflow = "", d || (c.style.display = "none"), "function" == typeof k && k()) : window.requestAnimationFrame(s) }) }
-
 /**
  * a Sidebar component
  * @param  {HTMLElement} el - sidebar element
@@ -27,7 +25,18 @@ class Sidebar {
       .forEach((el) => el.addEventListener("click", this.toggle.bind(this)))
     window.addEventListener("resize", this.onResize.bind(this))
 
-    //
+    
+    const toggleSubmenu = (el) => {
+      if (el.classList.contains("submenu-open")) {
+        el.classList.remove('submenu-open')
+        el.classList.add('submenu-closed')
+      } else {
+        el.classList.remove("submenu-closed")
+        el.classList.add("submenu-open")
+      } 
+    }
+
+
     let sidebarItems = document.querySelectorAll(".sidebar-item.has-sub")
     for (var i = 0; i < sidebarItems.length; i++) {
       let sidebarItem = sidebarItems[i]
@@ -37,16 +46,17 @@ class Sidebar {
           e.preventDefault()
 
           let submenu = sidebarItem.querySelector(".submenu")
-          if (submenu.classList.contains("submenu-open")) {
-            submenu.classList.remove('submenu-open')
-            submenu.classList.add('submenu-closed')
-          } else {
-            submenu.classList.remove("submenu-closed")
-            submenu.classList.add("submenu-open")
-          } 
-          // slideToggle(submenu, 300, () =>
-          //   this.forceElementVisibility(sidebarItem)
-          // )
+          toggleSubmenu(submenu)
+
+          // If submenu has submenu
+          const submenuItems = submenu.querySelectorAll('.submenu-item.has-sub') 
+          submenuItems.forEach(item => {
+            item.addEventListener('click', () => {
+              const submenuLevelTwo = item.querySelector('.submenu')
+              toggleSubmenu(submenuLevelTwo)
+            })
+          })
+          
         })
     }
 
@@ -82,10 +92,10 @@ class Sidebar {
       const sidebarItem = submenu.parentElement
       const height = submenu.clientHeight
       submenu.style.setProperty('--submenu-height', height + 'px')
-      submenu.style.height = 0
+      submenu.style.maxHeight = 0
 
-      if(!sidebarItem.classList.contains('active')) submenu.classList.add('submenu-closed')
-      else submenu.classList.add('submenu-open')
+      // if(!sidebarItem.classList.contains('active')) submenu.classList.add('submenu-closed')
+      // else submenu.classList.add('submenu-open')
     }
   }
 
