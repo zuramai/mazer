ae91f171d753ccc93a5b64d0062572b82aef3ee3
zuramai <saugi.dev@gmail.com>
2023-04-02 12:11:20 +0700

    fix: sweetalert2 dark mode inputs and colors

--- src/assets/static/js/pages/sweetalert2.js
+++ src/assets/static/js/pages/sweetalert2.js
@@ -1,8 +1,29 @@
+const Swal2 = Swal.mixin({
+  customClass: {
+    input: 'form-control'
+  }
+})
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
+
+
+
 document.getElementById("basic").addEventListener("click", (e) => {
-  Swal.fire("Any fool can use a computer")
+  Swal2.fire("Any fool can use a computer")
 })
 document.getElementById("footer").addEventListener("click", (e) => {
-  Swal.fire({
+  Swal2.fire({
     icon: "error",
     title: "Oops...",
     text: "Something went wrong!",
@@ -10,40 +31,40 @@ document.getElementById("footer").addEventListener("click", (e) => {
   })
 })
 document.getElementById("title").addEventListener("click", (e) => {
-  Swal.fire("The Internet?", "That thing is still around?", "question")
+  Swal2.fire("The Internet?", "That thing is still around?", "question")
 })
 document.getElementById("success").addEventListener("click", (e) => {
-  Swal.fire({
+  Swal2.fire({
     icon: "success",
     title: "Success",
   })
 })
 document.getElementById("error").addEventListener("click", (e) => {
-  Swal.fire({
+  Swal2.fire({
     icon: "error",
     title: "Error",
   })
 })
 document.getElementById("warning").addEventListener("click", (e) => {
-  Swal.fire({
+  Swal2.fire({
     icon: "warning",
     title: "Warning",
   })
 })
 document.getElementById("info").addEventListener("click", (e) => {
-  Swal.fire({
+  Swal2.fire({
     icon: "info",
     title: "Info",
   })
 })
 document.getElementById("question").addEventListener("click", (e) => {
-  Swal.fire({
+  Swal2.fire({
     icon: "question",
     title: "Question",
   })
 })
 document.getElementById("text").addEventListener("click", (e) => {
-  Swal.fire({
+  Swal2.fire({
     title: "Enter your IP address",
     input: "text",
     inputLabel: "Your IP address",
@@ -51,7 +72,7 @@ document.getElementById("text").addEventListener("click", (e) => {
   })
 })
 document.getElementById("email").addEventListener("click", async (e) => {
-  const { value: email } = await Swal.fire({
+  const { value: email } = await Swal2.fire({
     title: "Input email address",
     input: "email",
     inputLabel: "Your email address",
@@ -59,22 +80,22 @@ document.getElementById("email").addEventListener("click", async (e) => {
   })
 
   if (email) {
-    Swal.fire(`Entered email: ${email}`)
+    Swal2.fire(`Entered email: ${email}`)
   }
 })
 document.getElementById("url").addEventListener("click", async (e) => {
-  const { value: url } = await Swal.fire({
+  const { value: url } = await Swal2.fire({
     input: "url",
     inputLabel: "URL address",
     inputPlaceholder: "Enter the URL",
   })
 
   if (url) {
-    Swal.fire(`Entered URL: ${url}`)
+    Swal2.fire(`Entered URL: ${url}`)
   }
 })
 document.getElementById("password").addEventListener("click", async (e) => {
-  const { value: password } = await Swal.fire({
+  const { value: password } = await Swal2.fire({
     title: "Enter your password",
     input: "password",
     inputLabel: "Password",
@@ -87,11 +108,11 @@ document.getElementById("password").addEventListener("click", async (e) => {
   })
 
   if (password) {
-    Swal.fire(`Entered password: ${password}`)
+    Swal2.fire(`Entered password: ${password}`)
   }
 })
 document.getElementById("textarea").addEventListener("click", async (e) => {
-  const { value: text } = await Swal.fire({
+  const { value: text } = await Swal2.fire({
     input: "textarea",
     inputLabel: "Message",
     inputPlaceholder: "Type your message here...",
@@ -102,11 +123,11 @@ document.getElementById("textarea").addEventListener("click", async (e) => {
   })
 
   if (text) {
-    Swal.fire(text)
+    Swal2.fire(text)
   }
 })
 document.getElementById("select").addEventListener("click", async (e) => {
-  const { value: fruit } = await Swal.fire({
+  const { value: fruit } = await Swal2.fire({
     title: "Select field validation",
     input: "select",
     inputOptions: {
@@ -137,6 +158,26 @@ document.getElementById("select").addEventListener("click", async (e) => {
   })
 
   if (fruit) {
-    Swal.fire(`You selected: ${fruit}`)
+    Swal2.fire(`You selected: ${fruit}`)
   }
+})
+
+// Toasts
+document.getElementById('toast-success').addEventListener('click', () => {
+  Toast.fire({
+    icon: 'success',
+    title: 'Signed in successfully'
+  })
+})
+document.getElementById('toast-warning').addEventListener('click', () => {
+  Toast.fire({
+    icon: 'warning',
+    title: 'Please input your email'
+  })
+})
+document.getElementById('toast-failed').addEventListener('click', () => {
+  Toast.fire({
+    icon: 'error',
+    title: 'Transaction error. Please try again later'
+  })
 })
